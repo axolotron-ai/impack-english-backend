c068b7b6c14836d32c938a35e72aff93d6095ac1
Muthukumar <muthukumar69743@gmail.com>
2025-11-27 21:58:50 +0530

    env changes

--- db/index.js
+++ db/index.js
@@ -6,10 +6,15 @@ dotenv.config();
 
 const { Pool } = pkg;
 
+// export const pool = new Pool({
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   password: process.env.PG_PASSWORD,
+//   port: process.env.PG_PORT,
+// });
+
+//how to connect with connection string
 export const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
 });
